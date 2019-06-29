aaf56333b00d7144889474eaf8783794156046a2
Daniel Amarante <daniel.amarante2@gmail.com>
2019-06-29 14:33:22 -0300

    Start preparing view for svg

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,38 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Browser.Events exposing (onAnimationFrameDelta)
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Time
+
+
+
+---- CONSTANTS ----
+
+
+tickFrequency =
+    100
+
+
+gridSize =
+    Size 40 20
+
+
+cellSize =
+    Size 20 20
+
+
+type alias Size =
+    { width : Int
+    , height : Int
+    }
+
+
+type alias Position =
+    { x : Int
+    , y : Int
+    }
 
 
 
@@ -24,13 +54,13 @@ init =
 
 
 type Msg
-    = Frame Float
+    = Tick Time.Posix
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Frame _ ->
+        Tick time ->
             ( { model | count = model.count + 1 }, Cmd.none )
 
 
@@ -40,7 +70,29 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    text ("Count: " ++ String.fromFloat model.count)
+    svg
+        [ width "100%"
+        , height "auto"
+        , viewBox ("0 0 " ++ String.fromInt (gridSize.width * cellSize.width) ++ " " ++ String.fromInt (gridSize.height * cellSize.height))
+        ]
+        [ rect
+            [ width (String.fromInt (gridSize.width * cellSize.height))
+            , height (String.fromInt (gridSize.height * cellSize.height))
+            ]
+            []
+        , renderCircle "red" { x = 0, y = 0 }
+        ]
+
+
+renderCircle : String -> Position -> Html Msg
+renderCircle color pos =
+    circle
+        [ cx (String.fromInt ((pos.x * cellSize.width) + (cellSize.width // 2)))
+        , cy (String.fromInt ((pos.y * cellSize.height) + (cellSize.height // 2)))
+        , r (String.fromInt (cellSize.height // 2))
+        , fill color
+        ]
+        []
 
 
 
@@ -48,8 +100,8 @@ view model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    onAnimationFrameDelta Frame
+subscriptions model =
+    Time.every tickFrequency Tick
 
 
 
